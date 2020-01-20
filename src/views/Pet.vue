a2f28a93f9186e65bda554a123809e2b1343b066
Adam Santiaji <adam.wikrama@gmail.com>
2020-01-20 13:53:54 +0700

    update views

---
+++ src/views/Pet.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>{{ animal.name }} ({{ $route.params.species}})</h1>
+        <p>Age : {{ animal.age }} years old</p>
+        <p>Breed : {{ animal.breed }}</p>
+        <p>Color : {{animal.color}}</p>
+        <p>Gender : {{animal.gender}}</p>
+        <p>Location : {{animal.location}}</p>
+        <p>Weight : {{animal.weight}}</p>
+    </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+    data() {
+        return {
+            animal: {}
+        };
+    },
+    computed: {
+        ...mapState(["cats", "dogs"])
+    },
+    mounted() {
+        const animal = this[this.$route.params.species][this.$route.params.id];
+        this.animal = animal;
+    }
+};
+</script>
