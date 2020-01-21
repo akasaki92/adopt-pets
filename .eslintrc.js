4573fdd7543ba1142e482e956113aacf60fe7b14
Adam Santiaji <adam.wikrama@gmail.com>
2020-01-21 16:58:17 +0700

    update package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   'extends': [
+    'standard',
     'plugin:vue/essential',
     '@vue/standard'
   ],
+
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
   }
 }
